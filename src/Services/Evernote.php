83e20a2ce042bffe4cffba01a5c54a72f0821bca
Brian Faust <hello@brianfaust.de>
2017-06-06 08:04:06 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- src/Services/Evernote.php
+++ src/Services/Evernote.php
@@ -21,7 +21,7 @@ class Evernote extends Service
     public static function getFields($link, $text, $media)
     {
         return [
-            'url' => $link,
+            'url'   => $link,
             'title' => $text,
         ];
     }
